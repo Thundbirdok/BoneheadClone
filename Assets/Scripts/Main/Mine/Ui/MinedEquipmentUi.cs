e0b555cc5c1b237415079229aa334d407e6277ba
VyacheslavGorokhov <vyacheslavgorohov@mail.ru>
2023-08-02 15:52:40 +0700

    Add delta arrow animation

--- Assets/Scripts/Main/Mine/Ui/MinedEquipmentUi.cs
+++ Assets/Scripts/Main/Mine/Ui/MinedEquipmentUi.cs
@@ -95,7 +95,7 @@ namespace Main.Mine.Ui
             SetSetSlot(equipment);
             SetDropSlot(equipment);
 
-            SetParametersDelta();
+            SetParametersDelta(true);
             
             gameObject.SetActive(true);
 
@@ -170,7 +170,7 @@ namespace Main.Mine.Ui
         {
             (_set, _drop) = (_drop, _set);
 
-            SetParametersDelta();
+            SetParametersDelta(false);
 
             var setTargetPosition = _setSlotPosition;
             var dropTargetPosition = _dropSlotPosition;
@@ -203,41 +203,129 @@ namespace Main.Mine.Ui
             popupWindow.localScale = Vector3.zero;
         }
 
-        private void SetParametersDelta()
+        private void SetParametersDelta(bool isInitial)
         {
             var dropValue =  _drop == null ? 0 : _drop.Parameters.Sum(parameter => parameter.Value);
             var setValue = _set == null ? 0 : _set.Parameters.Sum(parameter => parameter.Value);
 
             var parametersDelta = dropValue - setValue;
 
-            var parametersDeltaTextValue = Mathf.Abs(parametersDelta).ToString();
+            var parametersDeltaValueText = Mathf.Abs(parametersDelta).ToString();
 
+            if (isInitial)
+            {
+                SetDeltaArrow(parametersDelta);
+            }
+            else
+            {
+                TweenDeltaArrow(parametersDelta);
+            }
+
+            parametersDeltaValueText = SetParametersDeltaText(parametersDeltaValueText, parametersDelta);
+
+            parametersDeltaText.text = parametersDeltaValueText;
+        }
+
+        private static string SetParametersDeltaText(string parametersDeltaTextValue, int parametersDelta)
+        {
+            parametersDeltaTextValue = parametersDelta switch
+            {
+                > 0 => "+" + parametersDeltaTextValue,
+                < 0 => "-" + parametersDeltaTextValue,
+                _   => parametersDeltaTextValue
+            };
+
+            return parametersDeltaTextValue;
+        }
+
+        private void SetDeltaArrow(int parametersDelta)
+        {
             switch (parametersDelta)
             {
                 case > 0:
-                    parametersDeltaTextValue = "+" + parametersDeltaTextValue;
-
+                    
                     arrow.transform.rotation = Quaternion.identity;
                     arrow.color = positiveArrowColor;
 
                     break;
 
                 case < 0:
-                    parametersDeltaTextValue = "-" + parametersDeltaTextValue;
-                
+                    
                     arrow.transform.rotation = Quaternion.Euler(0, 0, 180);
                     arrow.color = negativeArrowColor;
-
+                    
                     break;
 
                 default:
+
                     arrow.transform.rotation = Quaternion.Euler(0, 0, -90);
                     arrow.color = neutralArrowColor;
 
                     break;
             }
+        }
+
+        private void TweenDeltaArrow
+        (
+            int parametersDelta
+        )
+        {
+            var sequence = DOTween.Sequence();
             
-            parametersDeltaText.text = parametersDeltaTextValue;
+            switch (parametersDelta)
+            {
+                case > 0:
+                    
+                    if (Math.Abs(arrow.transform.rotation.eulerAngles.z - 180) < 1f
+                        || Math.Abs(arrow.transform.rotation.eulerAngles.z + 180) < 1f)
+                    {
+                        sequence
+                            .Append(arrow.transform.DORotate(new Vector3(0, 0, -90), swapDuration / 2))
+                            .Join(arrow.DOColor(neutralArrowColor, swapDuration / 2))
+                            .Append(arrow.transform.DORotate(Vector3.zero, swapDuration / 2))
+                            .Join(arrow.DOColor(positiveArrowColor, swapDuration / 2));
+                    }
+                    else
+                    {
+                        sequence
+                            .Append(arrow.transform.DORotate(Vector3.zero, swapDuration))
+                            .Join(arrow.DOColor(positiveArrowColor, swapDuration));
+                    }
+
+                    sequence.Play();
+
+                    break;
+
+                case < 0:
+
+                    if (Math.Abs(arrow.transform.rotation.eulerAngles.z) < 1f
+                        || Math.Abs(arrow.transform.rotation.eulerAngles.z - 360) < 1f
+                        || Math.Abs(arrow.transform.rotation.eulerAngles.z + 360) < 1f)
+                    {
+                        sequence
+                            .Append(arrow.transform.DORotate(new Vector3(0, 0, -90), swapDuration / 2))
+                            .Join(arrow.DOColor(neutralArrowColor, swapDuration / 2))
+                            .Append(arrow.transform.DORotate(new Vector3(0, 0, 180), swapDuration / 2))
+                            .Join(arrow.DOColor(negativeArrowColor, swapDuration / 2));
+                    }
+                    else
+                    {
+                        sequence
+                            .Append(arrow.transform.DORotate(new Vector3(0, 0, 180), swapDuration))
+                            .Join(arrow.DOColor(negativeArrowColor, swapDuration));
+                    }
+
+                    sequence.Play();
+
+                    break;
+
+                default:
+
+                    arrow.transform.DORotate(new Vector3(0, 0, -90), swapDuration);
+                    arrow.DOColor(neutralArrowColor, swapDuration);
+
+                    break;
+            }
         }
     }
 }
